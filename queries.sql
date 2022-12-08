fef86fa46cb06ee9613b787a618adac9026970ea
Yilikal250 <teshomeyilikal250@gmail.com>
2022-12-08 08:55:30 +0300

    Alter table and wrote a qurie

--- queries.sql
+++ queries.sql
@@ -6,3 +6,67 @@ SELECT name, escape_attempts, weight_kg FROM animals WHERE weight_kg > 10.5;
 SELECT name, neutered FROM animals WHERE neutered = 'TRUE';
 SELECT name FROM animals WHERE NOT name = 'Gabumon';
 SELECT name, weight_kg FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+COMMIT;
+SELECT * FROM animals;
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > DATE '2022-01-01';
+SAVEPOINT SP01;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO SP01;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
+SELECT * FROM animals;
+
+SELECT COUNT(*) AS "TOTAL ANIMALS" FROM animals;
+SELECT COUNT(*) AS "ANIMALS WITH 0 ESCAPE COUNT" FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) AS "AVERAGE WEIGHT OF ANIMALS" FROM animals;
+SELECT neutered, AVG(escape_attempts) AS "AVERAGE ESCAPE ATTEMPTS OF ANIMALS" FROM animals GROUP BY neutered ORDER BY AVG(escape_attempts) DESC LIMIT 1;
+SELECT species, MIN(weight_kg) AS "MIN WEIGHT", MAX(weight_kg) AS "MAX WEIGHT" FROM animals GROUP BY species;
+SELECT species, AVG(escape_attempts) AS "AVERAGE ESCAPE ATTEMPTS OF ANIMALS" FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-01-01' GROUP BY species;
+
+SELECT a.name AS "ANIMAL NAME", o.full_name AS "BELONG TO" FROM animals AS a 
+JOIN owners AS o 
+ON a.owner_id = o.id 
+WHERE o.full_name = 'Melody Pond'; 
+
+SELECT o.full_name AS "OWNER NAME", a.name AS "ANIMAL NAME" FROM owners AS o 
+LEFT JOIN animals AS a 
+ON o.id = a.owner_id;
+
+SELECT s.name AS "SPECIES", COUNT(a.species_id) "NUMBER OF ANIMALS PER SPECIES" FROM animals AS a 
+JOIN species AS s 
+ON a.species_id = s.id 
+GROUP BY s.id;
+
+SELECT o.full_name AS "OWNER NAME", a.name AS "ANIMAL NAME" FROM owners AS o 
+JOIN animals AS a 
+ON o.id = a.owner_id 
+WHERE o.full_name = 'Jennifer Orwell' AND a.species_id = 2;
+
+SELECT o.full_name AS "OWNER NAME", a.name AS "ANIMAL NAME", a.escape_attempts AS "ESCAPE ATTEMPTS" FROM owners AS o 
+JOIN animals AS a 
+ON o.id = a.owner_id 
+WHERE o.full_name = 'Dean Winchester' AND a.escape_attempts = 0;
+
+SELECT o.full_name AS "OWNER NAME", COUNT(a.name) as "NUMBER OF ANIMALS" FROM owners AS o 
+LEFT JOIN animals AS a 
+ON a.owner_id = o.id 
+GROUP BY o.full_name 
+ORDER BY COUNT(a.name) DESC 
+LIMIT 1;
