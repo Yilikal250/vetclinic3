252fa1bf0bb7beb53314b0e59b5da109c4353171
Yilikal250 <teshomeyilikal250@gmail.com>
2022-12-08 10:31:13 +0300

    updated tables and added quries

--- schema.sql
+++ schema.sql
@@ -1,13 +1,11 @@
 CREATE TABLE animals (
- id integer,
-name VARCHAR(100),
-id int,
-date_of_birth date,
-escape_attempts integer,
-neutered boolean,
-weight_kg decimal
+    id integer,
+    name text,
+    date_of_birth date,
+    escape_attempts integer,
+    neutered boolean,
+    weight_kg numeric
 );
-
 ALTER TABLE animals
 ADD COLUMN species TEXT;
 
@@ -36,3 +34,23 @@ ADD COLUMN owner_id INT;
 
 ALTER TABLE animals
 ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+CREATE TABLE vets (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    age INT,
+    date_of_graduation DATE
+);
+
+CREATE TABLE specializations (
+    vet_id INT REFERENCES vets (id),
+    species_id INT REFERENCES species (id)
+);
+
+CREATE TABLE visits (
+    animal_id INT REFERENCES animals (id),
+    vet_id INT REFERENCES vets (id),
+    date_of_visit DATE
+);
+
+ALTER TABLE owners ADD COLUMN email VARCHAR(120);
